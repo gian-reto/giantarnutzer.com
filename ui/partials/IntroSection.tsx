75aa9c2ae39729c420362f13aa9cd96d537a6563
Gian-Reto Tarnutzer <gian@cloudpages.ch>
2023-03-11 19:42:59 +0100

    Update IntroSection.tsx

--- ui/partials/IntroSection.tsx
+++ ui/partials/IntroSection.tsx
@@ -3,17 +3,17 @@ export const IntroSection = () => {
     <>
       <p className="text-xl font-normal !leading-relaxed sm:text-2xl md:text-4xl text-neutral-500 dark:text-neutral-500">
         <span className="text-neutral-900 dark:text-neutral-100">
-          I'm Gian, a software engineer from Basel, Switzerland.
+          I'm Gian, a software engineer based in Switzerland.
         </span>{" "}
         Currently working @{" "}
         <a
-          href="https://zemp.io"
+          href="https://threema.ch/en"
           className="underline transition-colors underline-offset-8 hover:text-neutral-900 dark:hover:text-neutral-100"
         >
-          Zemp Business Solutions AG
+          Threema
         </a>
-        . Perviously at Werbekontor GmbH & Vorsorgestiftung der Basler
-        Versicherung AG.
+        . Perviously at Zemp, Werbekontor, and Vorsorgestiftung der Basler
+        Versicherung.
       </p>
     </>
   );
